87668bd1b371517aecc4f9e675849bce3006b0ec
Nick Tillinghast <nick.tillinghast@thekrazycouponlady.com>
2021-12-20 11:46:44 -0700

    added a change to test feature branch

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/changed/i);
+  const linkElement = screen.getByText(/Deployed to aws yayyyyyy!!/i);
   expect(linkElement).toBeInTheDocument();
 });
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/changed/i);
+  const linkElement = screen.getByText(/Deployed to aws yayyyyyy!!/i);
   expect(linkElement).toBeInTheDocument();
 });
 
